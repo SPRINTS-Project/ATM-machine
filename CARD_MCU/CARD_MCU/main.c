9b40ba565154c269bd7262733192b88bbe587005
Matarawy <mahmoudsarhan02672@gmail.com>
2023-05-03 14:44:39 +0300

    add card mcu initial files

---
+++ CARD_MCU/CARD_MCU/main.c
@@ -0,0 +1,18 @@
+/*
+ * CARD_MCU.c
+ *
+ * Created: 5/3/2023 2:44:07 PM
+ * Author : engma
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
