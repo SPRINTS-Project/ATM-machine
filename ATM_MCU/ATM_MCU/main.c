c4d64e5fb7e4e05ce69700f13228d7943e70f34c
Matarawy <mahmoudsarhan02672@gmail.com>
2023-05-03 14:49:09 +0300

    add atm mcu initial files

---
+++ ATM_MCU/ATM_MCU/main.c
@@ -0,0 +1,18 @@
+/*
+ * ATM_MCU.c
+ *
+ * Created: 5/3/2023 2:48:21 PM
+ * Author : engma
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
