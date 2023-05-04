9f500921d67d87423511eb73cba954ddaa5e2871
mohamedhejazy74 <57074726+mohamedhejazy74@users.noreply.github.com>
2023-05-04 18:34:14 +0300

    add usart driver

---
+++ CARD_MCU/CARD_MCU/MCAL/usart/usart_config.h
@@ -0,0 +1,16 @@
+/*
+ * usart_config.h
+ *
+ * Created: 5/4/2023 5:19:37 PM
+ *  Author: Mcs
+ */ 
+
+
+#ifndef USART_CONFIG_H_
+#define USART_CONFIG_H_
+
+#ifndef	 F_CPU
+# define F_CPU 1000000UL
+#endif
+
+#endif /* USART_CONFIG_H_ */
--- CARD_MCU/CARD_MCU/STD_LIBRARIES/STD_TYPES.h
+++ CARD_MCU/CARD_MCU/STD_LIBRARIES/STD_TYPES.h
@@ -33,7 +33,7 @@
 
 
 /**********************************  section 4: Data Type Declarations  ******************************************/
-#if 0
+
 typedef unsigned char         uint8_t;          /*           0 .. 255             */
 typedef signed char           sint8_t;          /*        -128 .. +127            */
 typedef unsigned short        uint16_t;         /*           0 .. 65535           */
@@ -44,8 +44,6 @@ typedef unsigned long long    uint64_t;         /*       0..18446744073709551615
 typedef signed long long      sint64_t;
 typedef float                 float32_t;
 typedef double                float64_t;
-#endif
-
 
 typedef enum{
 	E_NOT_OK = 0,     
